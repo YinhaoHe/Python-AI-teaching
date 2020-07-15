6f1b8db6600cd20c147bca9de35ea7d3d6cce9c7
YinhaoHe <yinhaoh@uci.edu>
2020-07-15 16:23:50 -0700

    Update solution for 3.1

---
+++ solutions/3_solutions/3.1.py
@@ -0,0 +1,52 @@
+import numpy as np
+import pandas as pd
+
+data = pd.Series(np.random.randint(0, 100, 1000))
+print(data)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# import pandas as pd
+# import numpy as np
+# import random 
+# ls = []
+# for i in range(1000):
+#   a = random.randint(0,100)
+#   ls.append(a)
+
+
+
+# object = {
+#   'Random': [ls]
+# }
+# objects = pd.DataFrame(object)
+# print(objects.tail())
