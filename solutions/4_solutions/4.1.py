b9095c61b125551031dfdc078ccece8dedd9c9bf
YinhaoHe <yinhaoh@uci.edu>
2020-07-16 15:13:07 -0700

    Update solution for 4.1

---
+++ solutions/4_solutions/4.1.py
@@ -0,0 +1,34 @@
+import matplotlib.pyplot as plt
+X = range(1, 50)
+Y = [value * 3 for value in X]
+print("Values of X:")
+#print(*range(1,50)) 
+print("Values of Y (thrice of X):")
+print(Y)
+# Plot lines and/or markers to the Axes.
+plt.plot(X, Y)
+# Set the x axis label of the current axis.
+plt.xlabel('x - axis')
+# Set the y axis label of the current axis.
+plt.ylabel('y - axis')
+# Set a title 
+plt.title('Draw a line.')
+# Display the figure.
+plt.show()
+
+
+
+import matplotlib as mpl 
+mpl.use('Agg')
+import matplotlib.pyplot as plt
+
+x = [0, 3]
+y = [0, 1]
+
+plt.plot(x, y)
+
+plt.xlabel("x_axis")
+plt.ylabel("y_axis")
+
+plt.show()
+plt.savefig('graph.png')
